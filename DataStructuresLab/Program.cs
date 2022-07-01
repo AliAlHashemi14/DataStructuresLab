1db4d1b985d7f826cf35d11d1d1306ddeb6d8d62
AliAlHashemi14 <alialhashemi14@gmail.com>
2022-07-01 14:51:46 -0400

    updated

--- DataStructuresLab/Program.cs
+++ DataStructuresLab/Program.cs
@@ -1,13 +1,78 @@
-﻿//Request and take user input
-
-
+﻿//Using statements
 using System.Collections;
 
-Console.WriteLine("Hello, please enter a string:");
-var response = Console.ReadLine();
-Console.WriteLine(StringReverseUsingStack(response));
+//Running bool
+bool isrunning = true;
 
 
+do
+{
+    //Greet and prompt user
+    Console.WriteLine("Hello, please enter a string:");
+
+    //take response
+    string response = Console.ReadLine();
+
+    //variables
+    string[] sentence = response.Split(" ");
+    string result = "";
+
+    //Validate user input
+    try
+    {
+        while (true)
+        {
+
+            response = Console.ReadLine();
+            if (result.All(c => Char.IsLetter(c) == true))
+            {
+                //foreach loop to reverse sentence correctly
+                foreach (string s in sentence)
+                {
+                    result += StringReverseUsingStack(s) + " ";
+
+                }
+                Console.WriteLine(result);
+                break;
+            }
+            else
+            {
+                throw new Exception("Please do not enter any numbers or symbols. Try again");
+                continue;
+            }
+
+        }
+
+
+
+    }
+    catch (FormatException e)
+    {
+        Console.WriteLine(e.Message);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+
+    //Ask user if they'd like to loop again
+    Console.WriteLine("Would you like to try again? (y/n)");
+    if(Console.ReadLine().ToLower().Trim() == "y")
+    {
+        continue;
+    }
+    else
+    {
+        isrunning = false;
+    }
+}
+while (isrunning);
+
+
+
+
+
+//reverse method using Stack
  static string StringReverseUsingStack(string str)
 {
     var reversedStr = "";
