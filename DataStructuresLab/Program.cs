9a67b98114072bea40384f947a7e2f8755c72b8c
AliAlHashemi14 <alialhashemi14@gmail.com>
2022-07-01 14:15:11 -0400

    First commit

---
+++ DataStructuresLab/Program.cs
@@ -0,0 +1,20 @@
+﻿//Request and take user input
+
+
+using System.Collections;
+
+Console.WriteLine("Hello, please enter a string:");
+var response = Console.ReadLine();
+Console.WriteLine(StringReverseUsingStack(response));
+
+
+ static string StringReverseUsingStack(string str)
+{
+    var reversedStr = "";
+    Stack myStack = new Stack();
+    foreach(var t in str)
+    myStack.Push(t);
+    while(myStack.Count > 0)
+        reversedStr += myStack.Pop();
+    return reversedStr;
+}
